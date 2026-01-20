536811a7daddc25378df8a6a9409a6d058dc14d9
ruslan741 <mrqrczak1@yandex.ru>
2026-01-20 03:54:57 +0300

    new-look

--- .config/i3/scripts/autostart.sh
+++ .config/i3/scripts/autostart.sh
@@ -2,13 +2,14 @@
 # Autostart applications
 ## Polybar or tint
 ~/.config/i3/polybar/polybar-i3 &
+~/.config/i3/xborder/xborders &
 
 
 lxpolkit &
 dunst -config ~/.config/i3/dunst/dunstrc &
 picom --config ~/.config/i3/picom/picom.conf --animations -b &
 #picom-b &
-feh --bg-fill ~/.config/i3/wallpaper/wallhaven-rq7jw1_3440x1440.png &
+feh --bg-fill ~/.config/i3/wallpaper/wallhaven-gwj213.jpg &
 
 # sxhkd
 pkill -x sxhkd
