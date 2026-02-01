a3a984ba865090658fb9a35a4bee79ac858771c1
ruslan741 <mrqrczak1@yandex.ru>
2026-02-01 22:12:22 +0300

    noty

--- .config/i3/scripts/autostart.sh
+++ .config/i3/scripts/autostart.sh
@@ -1,13 +1,11 @@
 
 # Autostart applications
-## Polybar or tint
 ~/.config/i3/polybar/polybar-i3 &
-#~/.config/i3/xborder/xborders &
 
 
 
 lxpolkit &
-dunst -config ~/.config/i3/dunst/dunstrc &
+#dunst -config ~/.config/i3/dunst/dunstrc &
 picom --config ~/.config/i3/picom/picom.conf --animations -b &
 #picom-b &
 #feh --bg-fill ~/.config/i3/wallpaper/wallhaven-gwj213.jpg &
@@ -16,5 +14,4 @@ picom --config ~/.config/i3/picom/picom.conf --animations -b &
 pkill -x sxhkd
 
 sxhkd -c ~/.config/i3/sxhkd/sxhkdrc &
-pywalfox daemon &
 wal -R
